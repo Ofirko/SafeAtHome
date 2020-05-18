6a251b91c683ddc57cec146e777bd05197d60865
Sapir Yermiyau <sapiryer73@gmail.com>
2020-05-18 17:15:39 +0300

    second update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <div></div>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/landing.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className="col-md-4 col-xs-12">
+          <div></div>;<div></div>;<div></div>;
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Landing;
