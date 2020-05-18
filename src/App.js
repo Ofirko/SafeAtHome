63f2c6d93b6ddf30d3f2bd4ce9792d41a8019bb1
Ofir Katz <ofirkatz94@gmail.com>
2020-05-18 19:08:57 +0200

    basic routing and first components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { Components } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Landing from "./components/landing";
+import AdultAbuse from "./components/adultAbuse";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+      <Router>
+        <Switch>
+        <Route path="/questionnaire" exact component={Landing} />
+        <Route path="/questionnaire/step2" component={AdultAbuse} />
+        </Switch>
+      </Router>
+      </body>
+      <footer>
+      </footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Landing from "../src/components/landing";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Landing />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
