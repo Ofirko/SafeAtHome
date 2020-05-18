f192102ff55611bb5279832147add17475c047d8
Sapir Yermiyau <sapiryer73@gmail.com>
2020-05-18 17:47:36 +0300

    adding bootstrap

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 class Landing extends Component {
   state = {};
   render() {
     return (
-      <React.Fragment>
-        <div className="col-md-4 col-xs-12">
-          <div></div>;<div></div>;<div></div>;
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-5 col-lg-3 col-xl-3">a</div>
+          <div className="col-12 col-md-5 col-lg-3 col-xl-3">b</div>
+          <div className="col-12 col-md-5 col-lg-3 col-xl-3">c</div>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
