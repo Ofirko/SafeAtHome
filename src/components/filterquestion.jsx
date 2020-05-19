a2b63e187126d151b1cfe92284e561cad242a796
Ofir Katz <ofirkatz94@gmail.com>
2020-05-19 23:47:44 +0200

    fixing conflicts

---
+++ src/components/filterquestion.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/landing.scss";
+import QueHeader from "./que_header";
+import QueOption from "./que_option";
+class FilterQuestion extends Component {
+  state = {};
+
+  passToAdult=()=>{
+    this.props.history.push("/questionnaire/step2");
+  }
+
+  render() {
+    return (
+      <>
+        <QueHeader question="למה נכנסת לאפליקציה?" multipleAnswers={true} />
+        <form className="Options_container">
+          <QueOption optionText="ילד/ה שיתף/ה אותי במידע על פגיעה שחווה/תה "/>
+          <QueOption optionText="ראיתי משהו שהדאיג אותי לגבי ילד/ה "/>
+          <QueOption optionText="שיתפו אותי במידע לגבי ילד/ה אחר/ת שייתכן ועבר/ה פגיעה "/>
+        </form>
+      </>
+    );
+  }
+}
+
+export default FilterQuestion;
